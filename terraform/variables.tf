ba21e05ab893b38062c59798040547cd948a1f90
Jyo-Patil <jyopatil221@gmail.com>
2025-07-17 19:18:13 +0100

    bug fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,20 @@
 variable "aws_region" {
+  type    = string
   default = "us-east-1"
 }
 
+variable "az" {
+  type    = string
+  default = "us-east-1a"
+}
+
 variable "instance_type" {
+  type    = string
   default = "t2.micro"
+}
+
+# Tag (image tag in ECR) to deploy in Lambda
+variable "lambda_image_tag" {
+  type    = string
+  default = "latest"
 }
