2b0642471aba06ad11dea577b5dc9eed2cd59bfe
Jyo-Patil <jyopatil221@gmail.com>
2025-07-17 19:24:26 +0100

    bug fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "lambda_function_name" {
   value = aws_lambda_function.predictive_scaler.function_name
 }
+
+output "predictive_repo_url" {
+  description = "The URI of the ECR repository for the Lambda image"
+  value       = aws_ecr_repository.predictive_repo.repository_url
+}
